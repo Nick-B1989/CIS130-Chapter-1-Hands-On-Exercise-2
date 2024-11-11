1ba3d50d815172af60e52ac6aeb8fc39c714abbf
Nick-B1989 <VaalOdin@gmail.com>
2024-11-11 17:02:01 -0500

    This are the correct files

---
+++ project02/project01-02.js
@@ -0,0 +1,15 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Nick Baum
+    Date: 9/22/2024  
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+var service1Name = "Basic", service2Name = "Express"
+var service3Name = "Extreme", service4Name = "Ultimate"
+var service1Speed = "50 Mbps", service2Speed = "100 Mbps"
+var service3Speed = "500 Mbps", service4Speed = " 1 Gig"
